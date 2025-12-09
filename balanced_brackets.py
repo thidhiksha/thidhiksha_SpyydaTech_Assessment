f3699b8d50eff29c72a61cf0b667088a928b2ec9
thidhiksha <117349085+thidhiksha@users.noreply.github.com>
2025-12-09 12:41:36 +0530

    Update balanced_brackets.py

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -1,1 +1,22 @@
-
+#balanced brackets
+def check_brackets(s):
+  stack = []
+  for ch in s:
+    if ch in ['(','{','[']:
+      stack.append(ch)
+    elif ch in [')','}',']']:
+        if not stack:
+          return False
+        
+        last = stack.pop()
+        if ch==')'and last !='(':
+            return False
+        if ch=='}'and last !='{':
+            return False
+        if ch==']'and last!='[':
+            return False
+  #after checking,empty the stack
+  return len(stack)==0
+if __name__=="__main__":
+    print(check_brackets("{[()]}"))
+    print(check_brackets("([)]"))
